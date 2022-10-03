c97a7741c2fb4c76cfd2e91f47b9ae1d7afeebfe
ltabis <ltabis.pro@gmail.com>
2022-10-03 21:10:35 +0200

    docs: add crate dicumentation and clippy.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! # rhai-dylib - Load native Rust plugins using dynamic libraries !
+//!
+//! ![Rhai logo](https://rhai.rs/book/images/logo/rhai-banner-transparent-colour.svg)
+//!
+//! This crate is a really simple implementation of a dynamic library loader for Rhai.
+//! It provides traits to create plugins and plugin loaders.
+//!
+//! Check out the "simple" example to get started.
+
+#![deny(missing_docs)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+
 /// A plugin trait implementation.
 pub mod plugin;
 /// Trait implementation to create objects that load plugins.
