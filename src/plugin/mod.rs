eeebfa929e335e065c2d98cac43efd5911685a5b
ltabis <ltabis.pro@gmail.com>
2022-10-03 21:13:21 +0200

    docs: add missing documentation.

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -4,5 +4,6 @@ pub(crate) const PLUGIN_ENTRYPOINT: &str = "plugin_entrypoint";
 
 /// Trait used to register new rhai modules from a dynamic library.
 pub trait Plugin {
+    /// Update a rhai engine with whatever API the plugin exposes.
     fn register(&self, engine: &mut rhai::Engine);
 }
