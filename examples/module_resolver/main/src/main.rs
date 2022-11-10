66039cb28f7b73d6c7b8ffbd45e822c55044d102
ltabis <ltabis.pro@gmail.com>
2022-11-10 13:41:48 +0100

    chore(example): update module resolver example.

---
+++ examples/module_resolver/main/src/main.rs
@@ -0,0 +1,29 @@
+use rhai_dylib::rhai::{config::hashing::set_ahash_seed, ImmutableString};
+use rhai_dylib::{module_resolvers::libloading::DylibModuleResolver, rhai};
+
+fn main() {
+    if let Err(value) = set_ahash_seed(Some([1, 2, 3, 4])) {
+        panic!("ahash seed has been overridden by a plugin: {value:?}");
+    }
+
+    println!("main: {:?}", std::any::TypeId::of::<ImmutableString>());
+
+    let mut engine = rhai::Engine::new();
+
+    engine.set_module_resolver(DylibModuleResolver::new());
+
+    engine
+        .run(
+            r#"
+import "./target/debug/libplugin" as plugin;
+
+plugin::print_stuff();
+debug(plugin::triple_add(1, 2, 3));
+debug(plugin::get_property(#{ property: "value" }));
+
+let object = plugin::new_plugin_object("stuff to display");
+object.display_inner();
+"#,
+        )
+        .expect("failed to execute script");
+}
--- examples/module_resolver/plugin/src/lib.rs
+++ examples/module_resolver/plugin/src/lib.rs
@@ -1,8 +1,15 @@
 pub mod api;
 
-use rhai_dylib::rhai::{exported_module, Module, Shared};
+use rhai_dylib::rhai::{
+    config::hashing::set_ahash_seed, exported_module, ImmutableString, Module, Shared,
+};
 
 #[no_mangle]
 pub extern "C" fn module_entrypoint() -> Shared<Module> {
+    set_ahash_seed(Some([1, 2, 3, 4])).unwrap();
+
+    // Checking if TypeIDs are the same as the main program.
+    println!("plugin: {:?}", std::any::TypeId::of::<ImmutableString>());
+
     exported_module!(api::my_plugin_api).into()
 }
